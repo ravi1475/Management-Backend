0c1efa96a9d972b80a4ed941910881c211051614
Abhay <sharma727485@gmail.com>
2025-03-10 05:00:46 +0530

    change in  folder structure

--- src/routes/adminHandleRoute.js
+++ src/routes/adminHandleRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addAdmin } from "../controllers/auth/adminHandleController.js";
+import { addAdmin } from "../controllers/adminFun/adminHandleController.js";
 
 const router = express.Router();
 

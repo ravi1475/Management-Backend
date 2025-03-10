e1ac02078906823b132240cb59f8e9b2e785692e
Abhay <sharma727485@gmail.com>
2025-03-10 11:16:24 +0530

    abhay

--- src/routes/schoolAuthRoute.js
+++ src/routes/schoolAuthRoute.js
@@ -4,7 +4,6 @@ import {
   login,
   logout,
 } from "../controllers/auth/schoolAuthController.js";
-import { addSchool } from "../controllers/schoolFun/addSchoolController.js";
 
 const router = express.Router();
 
